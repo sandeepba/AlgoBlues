b36d29ee35e77865e20d6487b898fd01d0d10ee1
Sunny <sb3436@columbia.edu>
2013-12-07 15:39:51 -0500

    got the basic improvisation done for first line.

--- AlgoBlues.py
+++ AlgoBlues.py
@@ -20,37 +20,45 @@ class Melody:
 				long_enough = True
 			
 		#trim it down to the right size
-		while lily_length(new_measures)>2:
+		while lily_length(new_measures)> num_m:
 			new_measures = new_measures.split()
 			new_measures.pop()
 			new_measures = ' '.join(new_measures)
+			
+		#use rests to make up the difference
+		if lily_length(new_measures) < num_m:
+			beat_short = num_m - lily_length(new_measures)
+			correction = ' r' + str(int(1/beat_short))
+			new_measures += correction
+		
+		#allow for continued concatenation
+		new_measures += ' '
 		
 		#add it to the master record	
 		self.lily_string += new_measures
 		print self.lily_string
 		print lily_length(self.lily_string)
+		
+	def outputFile(self,fileName):
+		subprocess.call('cp template1.ly '+fileName+'.ly',shell=True)
+		subprocess.call('sed -i "s/PianoMelody/'+self.lily_string+\
+		'/g" '+fileName+'.ly', shell=True)
+		subprocess.call('lilypond '+fileName+'.ly',shell=True)
+		subprocess.call('timidity '+fileName+'.midi',shell=True)
+
 
 def lily_length(lily_notes):
 	note_lengths = []
+	print lily_notes.split()
 	for note in lily_notes.split():
+		print note
 		note_lengths.append(float(re.sub("\D", "", note)))	
 	return sum([1/x for x in note_lengths])
-	
-def write_to_file(instrumentNotes,fileName):
-	print instrumentNotes['piano']
-	subprocess.call('cp template1.ly '+fileName,shell=True)
-	subprocess.call(
-	'sed -i "s/SaxNotes/'+instrumentNotes['sax']+'/g" '+fileName,
-	shell=True)
-	subprocess.call(
-	'sed -i "s@PianoNotes@'+instrumentNotes['piano']+'@g" '+fileName,
-	shell=True)
-	subprocess.call('lilypond '+fileName,shell=True)
 
 def init_markov():
 	Key1 = MarkovChain("./markov1")
-	Key1.generateDatabase("g4 des8 c4. g8 bes8 r8 bes4. "+\
-	"g4 des8 c4. g8 bes8 r8 bes4. r2 r8 f8 fis8 g4")
+	Key1.generateDatabase("g''4 des''8 c''4 g'8 bes'8 r4 bes'4. r2 "+\
+      "g''4 des''8 c''4 g'8 bes'8 r2 r8 f'8 fis'8 g'8 ")
 	
 	Key2 = MarkovChain("./markov2")
 	Key2.generateDatabase("g4 des8 c4. g8 bes8 r8 bes4. "+\
@@ -70,6 +78,12 @@ def init_markov():
 def main():
 	markov_dict = init_markov()
 	blues_melody = Melody(markov_dict)		
+	blues_melody.addMeasures(4,'I')
 	blues_melody.addMeasures(2,'I')
+	blues_melody.addMeasures(2,'I')
+	blues_melody.addMeasures(1,'I')
+	blues_melody.addMeasures(1,'I')
+	blues_melody.addMeasures(2,'I')
+	blues_melody.outputFile('test1')
 
 main()
---
+++ template1.ly
@@ -0,0 +1,40 @@
+\score {
+  \new Staff <<
+    \new Voice {
+      \set midiInstrument = #"Electric Grand Piano"
+      \voiceOne
+      \time 4/4
+      PianoMelody
+       
+    }
+  \new Staff 
+    \new Voice {
+      \set midiInstrument = #"Electric Grand Piano"
+      \voiceTwo
+      \new Chordnames {
+      <bes d f aes>\breve
+      <bes d f aes>\breve
+      <ees g bes des>\breve
+      <bes d f aes>\breve
+      <c ees g bes>1
+      <f a c ees>1
+      <bes d f aes>\breve
+      }
+    }
+  >>
+  \layout { }
+  \midi {
+    \context {
+      \Staff
+      \remove "Staff_performer"
+    }
+    \context {
+      \Voice
+      \consists "Staff_performer"      
+    }
+    \context {
+      \Score
+      tempoWholesPerMinute = #(ly:make-moment 120 4)
+    }
+  }
+}
