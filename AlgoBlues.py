3da8a2321ab0c75a2094969118952156e8e7181d
Sunny <sb3436@columbia.edu>
2013-12-07 13:34:02 -0500

    Melody class generates strings.

--- AlgoBlues.py
+++ AlgoBlues.py
@@ -3,6 +3,15 @@
 import subprocess
 from pymarkovchain import MarkovChain
 
+class Melody:
+	def __init__(self,markov_dict):
+		self.markov_dict = markov_dict
+		self.lily_string = ""
+	
+	def addMeasures(self,num_m,key_m):
+		self.lily_string += self.markov_dict[key_m].generateString()
+		print self.lily_string
+
 def write_to_file(instrumentNotes,fileName):
 	print instrumentNotes['piano']
 	subprocess.call('cp template1.ly '+fileName,shell=True)
@@ -22,7 +31,7 @@ def init_markov():
 	Key2 = MarkovChain("./markov2")
 	Key2.generateDatabase("g4 des8 c4. g8 bes8 r8 bes4. "+\
 	"g4 des8 c4. g8 bes8 r8 bes4. r2 r8 f8 fis8 g4")
-	cu
+	
 	Key3 = MarkovChain("./markov3")
 	Key3.generateDatabase("g4 des8 c4. g8 bes8 r8 bes4. "+\
 	"g4 des8 c4. g8 bes8 r8 bes4. r2 r8 f8 fis8 g4")
@@ -32,13 +41,11 @@ def init_markov():
 	"g4 des8 c4. g8 bes8 r8 bes4. r2 r8 f8 fis8 g4")
 	
 	return {'I':Key1,'II':Key2,'III':Key3,'IV':Key4}
-	
-class Melody:
-	def __init__(self,markov_dict):
-		self.markov_dict = markov_dict
+			
 	
 def main():
 	markov_dict = init_markov()
-	blues_melody = Melody(markov_dict)	
-	
+	blues_melody = Melody(markov_dict)		
+	blues_melody.addMeasures(2,'I')
+
 main()
