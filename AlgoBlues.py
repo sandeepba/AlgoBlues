0a74409cd98145006d57a67989c21eb2b173c112
Sunny <sb3436@columbia.edu>
2013-12-07 12:07:39 -0500

    Now generating markov strings.

--- AlgoBlues.py
+++ AlgoBlues.py
@@ -15,38 +15,30 @@ def write_to_file(instrumentNotes,fileName):
 	subprocess.call('lilypond '+fileName,shell=True)
 
 def init_markov():
-	Key1 = MarkovChain("./markov")
+	Key1 = MarkovChain("./markov1")
 	Key1.generateDatabase("g4 des8 c4. g8 bes8 r8 bes4. "+\
 	"g4 des8 c4. g8 bes8 r8 bes4. r2 r8 f8 fis8 g4")
 	
-	Key2 = MarkovChain("./markov")
+	Key2 = MarkovChain("./markov2")
 	Key2.generateDatabase("g4 des8 c4. g8 bes8 r8 bes4. "+\
 	"g4 des8 c4. g8 bes8 r8 bes4. r2 r8 f8 fis8 g4")
 	
-	Key3 = MarkovChain("./markov")
+	Key3 = MarkovChain("./markov3")
 	Key3.generateDatabase("g4 des8 c4. g8 bes8 r8 bes4. "+\
 	"g4 des8 c4. g8 bes8 r8 bes4. r2 r8 f8 fis8 g4")
 	
-	Key4 = MarkovChain("./markov")
+	Key4 = MarkovChain("./markov4")
 	Key4.generateDatabase("g4 des8 c4. g8 bes8 r8 bes4. "+\
 	"g4 des8 c4. g8 bes8 r8 bes4. r2 r8 f8 fis8 g4")
 	
 	return {'I':Key1,'II':Key2,'III':Key3,'IV':Key4}
-	 
-def improv_section(section_key):
-	mcSax = MarkovChain("./markov")	
-	mcSax.generateDatabase(
-	"g4 des8 c4. g8 bes8 r8 bes4. r2 g4 des8 c4. g8 bes8 r2 r8 f8 fis8 g4 bes8 c8 b8 des8 c8 bes8 g c8 bes4. r2 r8 g8 c8 c8 d8 d8 ees4 e8 f4 f8 fis8 fis8 g4 a8 bes8"
-	)
-	write_to_file({'piano':'r1',
-				   'sax':mcSax.generateString()+mcSax.generateString()
-				   +mcSax.generateString()+mcSax.generateString()
-				   +mcSax.generateString()+mcSax.generateString()},
-				   'test7.ly')
 	
 def main():
-	init_markov()
-	
+	markov_dict = init_markov()
+	print markov_dict['I'].generateString()
+	print markov_dict['II'].generateString()
+	print markov_dict['III'].generateString()
+	print markov_dict['IV'].generateString()
 		
 	
 main()
